25dd667cd12be38466da1acaba2d10e487d7eef6
Brad Francis <branci@siue.edu>
2018-06-18 20:40:36 -0500

    spring data starter

---
+++ src/main/java/com/daugherty/postmandemo/postmandemo/PostmanDemoApplication.java
@@ -0,0 +1,12 @@
+package com.daugherty.postmandemo.postmandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmanDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostmanDemoApplication.class, args);
+	}
+}
